655be3ade92f11a02aab933ab095ea8975f82802
Vrushali Panchal <vrushalipanchal31@gmail.com>
2026-08-21 17:23:18 +0530

    Initial Commit

--- clothing-store/backend/api/admin.py
+++ clothing-store/backend/api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, Product, ProductImage, Order, OrderItem, Wishlist
+from .models import Category, Product, ProductImage, Order, OrderItem, Wishlist, Review
 
 class ProductImageInline(admin.TabularInline):
     model = ProductImage
@@ -24,3 +24,4 @@ admin.site.register(Category)
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Order, OrderAdmin)
 admin.site.register(Wishlist)
+admin.site.register(Review)
