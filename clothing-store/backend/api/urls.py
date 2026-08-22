226f3fb5b14537d15d62774a97f55605e8f3dd02
Vrushali Panchal <vrushalipanchal31@gmail.com>
2026-08-22 22:34:11 +0530

    Initial Commit

--- clothing-store/backend/api/urls.py
+++ clothing-store/backend/api/urls.py
@@ -1,39 +1,3 @@
-# from django.urls import path, include
-# from rest_framework.routers import DefaultRouter
-# from .views import CategoryViewSet, ProductViewSet, OrderViewSet, WishlistViewSet
-
-# router = DefaultRouter()
-# router.register(r'categories', CategoryViewSet, basename='category')
-# router.register(r'products', ProductViewSet, basename='product')
-# router.register(r'orders', OrderViewSet, basename='order')
-# router.register(r'wishlist', WishlistViewSet, basename='wishlist')
-
-# urlpatterns = [
-#     path('', include(router.urls)),
-# ]
-
-
-# from django.urls import path, include
-# from rest_framework.routers import DefaultRouter
-# from .views import CategoryViewSet, ProductViewSet, OrderViewSet, WishlistViewSet, ReviewViewSet
-
-# router = DefaultRouter()
-# router.register(r'categories', CategoryViewSet, basename='category')
-# router.register(r'products', ProductViewSet, basename='product')
-# router.register(r'orders', OrderViewSet, basename='order')
-# router.register(r'wishlist', WishlistViewSet, basename='wishlist')
-# router.register(r'reviews', ReviewViewSet, basename='review')
-
-# urlpatterns = [
-#     path('', include(router.urls)),
-# ]
-
-
-
-
-
-
-
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
--- clothing-store/backend/core/urls.py
+++ clothing-store/backend/core/urls.py
@@ -1,27 +1,3 @@
-# from django.contrib import admin
-# from django.urls import path, include
-# from django.conf import settings
-# from django.conf.urls.static import static
-# from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-# from api.views import RegisterView
-
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     path('api/', include('api.urls')), # અહીં ક્યાંય 'core.urls' ના હોવું જોઈએ
-#     path('api/register/', RegisterView.as_view(), name='register'),
-#     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-#     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-# ]
-
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
-
-
-
-
-
 from django.contrib import admin
 from django.urls import path, include
 
